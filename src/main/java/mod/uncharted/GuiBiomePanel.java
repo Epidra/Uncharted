8fde806e13dd60fff2071b7cb222ee544ca917ba
Epidra <epidra@web.de>
2020-02-12 11:49:50 +0100

    -

--- src/main/java/mod/uncharted/GuiBiomePanel.java
+++ src/main/java/mod/uncharted/GuiBiomePanel.java
@@ -1,13 +1,13 @@
 package mod.uncharted;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.client.renderer.texture.ITextureObject;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Biomes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -15,24 +15,44 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.lwjgl.opengl.GL11;
 
-public class GuiBiomePanel extends Gui {
+import java.util.Random;
+
+public class GuiBiomePanel extends AbstractGui {
 
     /** Minecraft Instance */
     private Minecraft mc;
     /** Biome the Player is currently in */
-    Biome biomePanel;
+    private Biome biomePanel;
     /** String version of the current Biome */
-    ResourceLocation biomeTexture;
+    private ResourceLocation biomeTexture;
+    /** String version of the Static */
+    private ResourceLocation biomeTextureStatic;
+    /** String version of the Overlay */
+    private ResourceLocation biomeTextureOverlay;
     /** Trigger if Panel moves up or down */
-    boolean transitionUp;
+    private boolean transitionUp;
     /** Upward Position of Panel */
-    float timer;
+    private float timer;
     /** Absolute X Position of Panel (upper left edge) */
-    int posX;
+    private int posX;
     /** Absolute Y Position of Panel (upper left edge) */
-    int posY;
+    private int posY;
     /** Maximum Upward Position */
-    int timerMax;
+    private int timerMax;
+    /** Dissected String of Biome Name */
+    private String[] biomeName = new String[]{"", "", ""};
+    /** Localized String for ENTERING message **/
+    private String entering = "ENTERING...WORLD";
+
+    private boolean smallFrame = true;
+    private float scale = 1.0f;
+    private int height = 256;
+    private boolean borderLower = true;
+    private boolean borderLeft = false;
+
+    private boolean animated = false;
+    private String[] stringlist = new String[]{"beach"};
+    private int listindex = 0;
 
 
 
@@ -48,9 +68,36 @@ public class GuiBiomePanel extends Gui {
         timerMax = 200;
         posX  = 0;
         posY  = 0;
-        biomeTexture = new ResourceLocation(Uncharted.MODID + ":" + "textures/uncharted.png");
+        biomeTexture        = new ResourceLocation(Uncharted.MODID, "textures/static.png");
+        biomeTextureStatic  = new ResourceLocation(Uncharted.MODID, "textures/static.png");
+        biomeTextureOverlay = new ResourceLocation(Uncharted.MODID, "textures/overlay.png");
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onTravel);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onRenderExperienceBar);
+        smallFrame = UnchartedConfig.smallFrame;
+        scale = (float)UnchartedConfig.scale/100.00f;
+        borderLower = UnchartedConfig.borderLower;
+        borderLeft = UnchartedConfig.borderLeft;
+        if(smallFrame){
+            height = 32;
+            timerMax -=56;
+        } else {
+            height = 64;
+        }
+    }
+
+    private void LoadBiome(){
+        ITextureObject texture = mc.getTextureManager().getTexture(new ResourceLocation(Uncharted.MODID , "textures/biomes/" + stringlist[listindex] + ".png"));
+        if(texture instanceof DynamicTexture) { // texture file not found
+            listindex++;
+            if (listindex == stringlist.length) {
+                biomeTexture = biomeTextureStatic;
+                animated = true;
+                listindex = 0;
+            }
+        } else {
+            biomeTexture = new ResourceLocation(Uncharted.MODID , "textures/biomes/" + stringlist[listindex] + ".png");
+            animated = false;
+        }
     }
 
 
@@ -61,7 +108,7 @@ public class GuiBiomePanel extends Gui {
     @SubscribeEvent
     public void onTravel(LivingEvent.LivingUpdateEvent event){
         boolean newBiome = false;
-        if(event.getEntity() instanceof EntityPlayer){
+        if(event.getEntity() instanceof PlayerEntity){
             if(event.getEntity().dimension == DimensionType.OVERWORLD){ // Checks for Overworld
                 if(event.getEntity().world.canBlockSeeSky(event.getEntity().getPosition())){
                     if(biomePanel != event.getEntity().world.getBiome(event.getEntity().getPosition())){
@@ -77,7 +124,19 @@ public class GuiBiomePanel extends Gui {
         if(newBiome){ // trigger for when we travel into a new Biome
             biomePanel = event.getEntity().world.getBiome(event.getEntity().getPosition());
             transitionUp = true;
-            biomeTexture = new ResourceLocation(Uncharted.MODID , "textures/" + biomePanel.getRegistryName().getPath() + ".png");
+            String translatedKey = net.minecraft.client.resources.I18n.format(biomePanel.getTranslationKey());
+            entering = net.minecraft.client.resources.I18n.format("gui.uncharted.entering");
+            biomeName = translatedKey.split(" ");
+            String[] templist = biomePanel.getRegistryName().getPath().split("_");
+            if(templist.length > 1){
+                stringlist = new String[templist.length + 1];
+                stringlist[0] = biomePanel.getRegistryName().getPath();
+                System.arraycopy(templist, 0, stringlist, 1, templist.length);
+            } else {
+                stringlist = new String[1];
+                stringlist[0] = biomePanel.getRegistryName().getPath();
+            }
+            listindex = 0;
         }
     }
 
@@ -89,54 +148,56 @@ public class GuiBiomePanel extends Gui {
             return;
         }
 
-        int fps = Minecraft.getDebugFPS();
-        if(fps == 0){
-            fps = 1;
-        } else {
-            fps = 120 / fps;
-        }
-
-
         if(transitionUp){
             if(timer >= timerMax){
                 timer = timerMax;
                 transitionUp = false;
             } else {
                 timer += 1;
-                //timer += fps;
-                //timer += event.getPartialTicks()/2;
             }
         } else {
             if(timer <= 0){
                 timer = 0;
             } else {
                 timer -= 1;
-                //timer -= fps;
-                //timer -= event.getPartialTicks()/2;
             }
         }
 
         if(timer > 0){
-            posX = mc.mainWindow.getScaledWidth()-10;
-            posY = mc.mainWindow.getScaledHeight();
-            int u = (int)(posY-timer/2);
-            if(timer > 130){
-                u = posY-130/2;
-            }
+            LoadBiome();
+            int v = 150;
+            if(smallFrame) v-=56;
+            int u = timer > v ? v/2 : (int)(timer/2);
+            posX = borderLeft ? 10 : mc.mainWindow.getScaledWidth()-10-128;
+            posY = borderLower ? (int)(mc.mainWindow.getScaledHeight()-u+8) : -(height+10)+u;
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             GL11.glDisable(GL11.GL_LIGHTING);
-            ITextureObject texture = mc.getTextureManager().getTexture(biomeTexture);
-            if(texture instanceof DynamicTexture){
-                biomeTexture = new ResourceLocation(Uncharted.MODID, "textures/uncharted.png");
-                this.mc.getTextureManager().bindTexture(biomeTexture);
-                this.drawTexturedModalRect(posX-120, u, 0, 0, 120, 64);
-            }else if(texture == null){
-                this.mc.getTextureManager().bindTexture(biomeTexture);
-                this.drawTexturedModalRect(posX-120, u, 0, 0, 120, 64);
-            }else{
-                this.mc.getTextureManager().bindTexture(biomeTexture);
-                this.drawTexturedModalRect(posX-120, u, 0, 0, 120, 64);
+            this.mc.getTextureManager().bindTexture(biomeTextureOverlay);
+            this.blit(posX-4, posY-4, 0,smallFrame ? 128 : 0, 128+8, 64+8);
+            this.mc.getTextureManager().bindTexture(biomeTexture);
+            if(animated){
+                Random r = new Random();
+                this.blit(posX, posY, r.nextInt(128), r.nextInt(256-64), 128, height);
+            } else {
+                this.blit(posX, posY + (smallFrame ? 1 : 0), 0, smallFrame ? 16 : 0, 128, height);
             }
+            GL11.glPushMatrix();
+            GL11.glScalef(scale, scale, scale); {
+                DrawString(entering, posX + 2, posY + 2, false);
+                for(int i = 0; i < biomeName.length; i++){
+                    DrawString(biomeName[i], posX + 124, posY + height - 10*biomeName.length + i*10, true);
+                }
+            } GL11.glPopMatrix();
+        }
+    }
+
+    private void DrawString(String text, int posX, int posY, boolean rightsided){
+        if(rightsided){
+            drawRightAlignedString(mc.fontRenderer, text, (int)((posX    )/scale), (int)((posY    )/scale), 0);
+            drawRightAlignedString(mc.fontRenderer, text, (int)((posX + 1)/scale), (int)((posY + 1)/scale), 16777215);
+        } else {
+            drawString(mc.fontRenderer, text, (int)((posX    )/scale), (int)((posY   )/scale), 0);
+            drawString(mc.fontRenderer, text, (int)((posX + 1)/scale), (int)((posY + 1)/scale), 16777215);
         }
     }
 
--- src/main/java/mod/uncharted/Uncharted.java
+++ src/main/java/mod/uncharted/Uncharted.java
@@ -1,28 +1,18 @@
 package mod.uncharted;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 @Mod("uncharted")
 public class Uncharted {
 
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
     /** The Mod ID */
     public static final String MODID = "uncharted";
 
@@ -34,6 +24,7 @@ public class Uncharted {
     public Uncharted() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         MinecraftForge.EVENT_BUS.register(this);
+        UnchartedConfig.init();
     }
 
 
@@ -42,6 +33,7 @@ public class Uncharted {
 
     /** Client Side Setup Event */
     private void doClientStuff(final FMLClientSetupEvent event) {
+        UnchartedConfig.load();
         MinecraftForge.EVENT_BUS.register(new GuiBiomePanel(Minecraft.getInstance()));
     }
 }
